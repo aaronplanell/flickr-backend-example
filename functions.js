a545ce30c7338b7a273726cfe0deaccd72071bc7
Aaron Planell <aaronplanell@gmail.com>
2017-03-11 17:55:36 +0100

    Create an object with the connection data.

--- functions.js
+++ functions.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 //Constants
 import { FLICKR_API_URL, DEFAULT_PARAMS } from './constants';
 
-//Call to
-export const generateApiFlickCall = (method, params) => {
+//Call to Flickr without authentication
+export const generateApiFlickCallWithoutAuthentication = (method, params) => {
   return (req, res) => {
     axios.get(FLICKR_API_URL, {
       params: {
@@ -21,3 +21,34 @@ export const generateApiFlickCall = (method, params) => {
     });
   }
 };
+
+//Construct an object with all the data of the connection
+export const getConnectionData = (req) => {
+  if (req && req.session && req.session.grant && req.session.grant.step1 && req.session.grant.response) {
+    const { oauth_token, oauth_token_secret } = req.session.grant.step1;
+    const { access_token, access_secret, raw } = req.session.grant.response;
+    const { user_nsid, username, fullname } = raw;
+    const connectionData = { user_nsid, username, fullname, oauth_token, oauth_token_secret, access_token, access_secret };
+    showConnectionData(connectionData);
+    return connectionData;
+  } else {
+    return null;
+  }
+}
+
+/*
+ * Show data for debugging purposes
+ * Receives and object with this structure: user_nsid, username, fullname, oauth_token, oauth_token_secret, access_token, access_secret
+ ***/
+const showConnectionData = (connectionData) => {
+  const { user_nsid, username, fullname, oauth_token, oauth_token_secret, access_token, access_secret } = connectionData;
+  console.log('--------------------- Connection Data ---------------------');
+  console.log(' - user_nsid____________: ', user_nsid);
+  console.log(' - username_____________: ', username);
+  console.log(' - fullname_____________: ', fullname);
+  console.log(' - oauth_token__________: ', oauth_token);
+  console.log(' - oauth_token_secret___: ', oauth_token_secret);
+  console.log(' - access_token_________: ', access_token);
+  console.log(' - access_secret________: ', access_secret);
+  console.log('-----------------------------------------------------------');
+}
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import { APPLICATION_PORT, FLICKR_CONSUMER_SECRET } from './constants';
 import { config } from './config';
 
 //Auxiliar functions
-import { getAccessToken, generateApiFlickCall } from './functions';
+import { getConnectionData, generateApiFlickCallWithoutAuthentication } from './functions';
 
 //App Express
 var app = express();
@@ -21,17 +21,27 @@ let currentSession;
 
 //The main page
 app.get('/', function (req, res) {
+
   //If there are no session, must connect
   if (!currentSession) {
     res.redirect('/connect/flickr');
   }
   else {
-    //OK. If we are here we are logged :D
-    app.get('/methods', generateApiFlickCall('flickr.reflection.getMethods', {}));
-    app.get('/photos', generateApiFlickCall('flickr.photos.search', {user_id: '148575064@N08'}));
-    app.get('/collections', generateApiFlickCall('flickr.collections.getTree', {user_id: '148575064@N08'}));
 
-    res.end(currentSession);
+    //OK. If we are here we are logged :D
+    const connectionData = getConnectionData(req);
+
+    if (connectionData) {
+
+      //Configure the call for those methods that don't need authentication
+      app.get('/photos', generateApiFlickCallWithoutAuthentication('flickr.photos.search', {user_id: '148575064@N08'}));
+      app.get('/collections', generateApiFlickCallWithoutAuthentication('flickr.collections.getTree', {user_id: '148575064@N08'}));
+
+      //Configure the call for thos methods that NEED authentication
+      res.end('Well... You\'re connected. You can work with the API :D');
+    } else {
+      res.end('Ooops! Something wrong has happened :/');
+    }
   }
 })
 
